48c38ea6d9a534b8eb19966e31c9001b9c1e4e84
Tomasz Cudziło <cudzilot@ee.pw.edu.pl>
2016-07-06 21:38:36 +0200

    refactor some constants in Catalog specs

--- spec/catalog_spec.rb
+++ spec/catalog_spec.rb
@@ -1,17 +1,16 @@
 require_relative '../catalog'
 require_relative '../product'
 
-VALID_PRODUCT_ID = 1
-INVALID_PRODUCT_ID = -1
-
 RSpec.describe Catalog do
   context 'when asked for existing product' do
+    let(:product_id) { 1 }
+
     it 'confirms the product exists' do
-      expect(Catalog.has?(VALID_PRODUCT_ID)).to be_truthy
+      expect(Catalog.has?(product_id)).to be_truthy
     end
 
     it 'returns a product' do
-      expect(Catalog.find(1)).to be_a(Product)
+      expect(Catalog.find(product_id)).to be_a(Product)
     end
 
     it 'returns the product' do
@@ -21,17 +20,19 @@ RSpec.describe Catalog do
         price: 2800,
         vat_category_id: 2
       )
-      expect(Catalog.find(VALID_PRODUCT_ID)).to eq(identical_product)
+      expect(Catalog.find(product_id)).to eq(identical_product)
     end
   end
 
   context 'when asked for non-existing product' do
+    let(:product_id) { -1 }
+
     it 'denies the product exists' do
-      expect(Catalog.has?(INVALID_PRODUCT_ID)).to be_falsey
+      expect(Catalog.has?(product_id)).to be_falsey
     end
 
     it 'returns nil' do
-      expect(Catalog.find(INVALID_PRODUCT_ID)).to be_nil
+      expect(Catalog.find(product_id)).to be_nil
     end
   end
 end
