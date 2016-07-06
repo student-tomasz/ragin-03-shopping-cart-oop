c7c2fc0244ddefae881d2e72eae4478a83f780be
Tomasz Cudziło <cudzilot@ee.pw.edu.pl>
2016-07-06 21:52:28 +0200

    refactor constants in CartItem specs

--- spec/cart_item_spec.rb
+++ spec/cart_item_spec.rb
@@ -1,25 +1,21 @@
 require_relative '../cart_item'
 
-INVALID_PRODUCT_ID = -1
-VALID_PRODUCT_ID = 1
-VALID_PRODUCT_HASH = {
-  id: 1,
-  name: 'Agile Web Development with Rails 5',
-  price: 2800,
-  price_with_vat: 3024,
-  vat: 0.08
-}
-
 RSpec.describe CartItem do
-  it 'raises error when created for invalid product' do
-    expect { CartItem.new INVALID_PRODUCT_ID }.to raise_error(ArgumentError)
+  context 'when created for an invalid product' do
+    let(:product_id) { -1 }
+
+    it 'raises error' do
+      expect { CartItem.new(product_id) }.to raise_error(ArgumentError)
+    end
   end
 
   context 'when created for a valid product' do
-    subject(:item) { CartItem.new VALID_PRODUCT_ID }
+    let(:product_id) { 1 }
+
+    subject(:item) { CartItem.new(product_id) }
 
     it 'contains the passed product' do
-      expect(item.product.id).to eq(VALID_PRODUCT_ID)
+      expect(item.product.id).to eq(product_id)
     end
 
     it 'has zero quantity' do
@@ -52,7 +48,13 @@ RSpec.describe CartItem do
 
     it 'returns correct hash' do
       expect(item.to_h).to eq(
-        product: VALID_PRODUCT_HASH,
+        product: {
+          id: 1,
+          name: 'Agile Web Development with Rails 5',
+          price: 2800,
+          price_with_vat: 3024,
+          vat: 0.08
+        },
         quantity: 0,
         total: 0,
         total_with_vat: 0
@@ -60,32 +62,40 @@ RSpec.describe CartItem do
     end
 
     context 'when twice incremented' do
-      subject(:item) do
-        item = CartItem.new VALID_PRODUCT_ID
+      subject(:incremented_item) do
         item.increment
         item.increment
         item
       end
 
       it 'has quantity value of two' do
-        expect(item.quantity).to eq(2)
+        expect(incremented_item.quantity).to eq(2)
       end
 
       it 'decrements quantity by one at a time' do
-        expect { item.decrement }. to change { item.quantity }.from(2).to(1)
+        expect { incremented_item.decrement }
+          .to change { incremented_item.quantity }
+          .from(2)
+          .to(1)
       end
 
       it 'has a total of exactly two books\' price' do
-        expect(item.total).to eq(5600)
+        expect(incremented_item.total).to eq(5600)
       end
 
       it 'has a total with vat of exactly two books\' price with vat' do
-        expect(item.total_with_vat).to eq(6048)
+        expect(incremented_item.total_with_vat).to eq(6048)
       end
 
       it 'returns updated hash' do
-        expect(item.to_h).to eq(
-          product: VALID_PRODUCT_HASH,
+        expect(incremented_item.to_h).to eq(
+          product: {
+            id: 1,
+            name: 'Agile Web Development with Rails 5',
+            price: 2800,
+            price_with_vat: 3024,
+            vat: 0.08
+          },
           quantity: 2,
           total: 5600,
           total_with_vat: 6048
