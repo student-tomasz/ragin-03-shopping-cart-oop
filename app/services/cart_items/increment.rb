1e98b99913f9de7276533372c0dcb518481b5209
Tomasz Cudziło <cudzilot@ee.pw.edu.pl>
2016-07-15 13:36:10 +0200

    Refactor service incrementing cart items

--- app/services/cart_items/increment.rb
+++ app/services/cart_items/increment.rb
@@ -2,14 +2,33 @@ module Shop
   module Services
     module CartItems
       class Increment
+        InvalidProductIdError = Class.new(ArgumentError)
+
         def call(product_id:)
           cart_item = Fetch.new.call(product_id: product_id)
           if cart_item
-            cart_item.quantity += 1
+            set_quantity(product_id, cart_item.quantity + 1)
           else
-            cart_item = Models::CartItem.new(product_id: product_id, quantity: 1)
-            CART_ITEMS << cart_item
+            create_new_cart_item!(product_id)
           end
+        end
+
+        private
+
+        def set_quantity(product_id, quantity)
+          SetQuantity.new.call(
+            product_id: product_id,
+            quantity: quantity
+          )
+        end
+
+        def create_new_cart_item!(product_id)
+          cart_item = begin
+            Models::CartItem.new(product_id: product_id, quantity: 1)
+          rescue
+            raise InvalidProductIdError
+          end
+          CART_ITEMS << cart_item
           cart_item
         end
       end
